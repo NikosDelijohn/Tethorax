7d3c93fa8f6c1bfc89311a4568b0fa6f057fcbb4
Nick Deligiannis <nik.deligi@gmail.com>
2019-08-29 23:56:09 +0300

    :wrench: IF's Instruction Cache

--- TOOLBOX Components/IF_INSTRMEM.vhd
+++ TOOLBOX Components/IF_INSTRMEM.vhd
@@ -42,7 +42,7 @@ USE altera_mf.all;
 ENTITY IF_INSTRMEM IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (6 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (9 DOWNTO 0);
 		clken		: IN STD_LOGIC  := '1';
 		clock		: IN STD_LOGIC  := '1';
 		data		: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
@@ -80,7 +80,7 @@ ARCHITECTURE SYN OF if_instrmem IS
 			clocken0	: IN STD_LOGIC ;
 			wren_a	: IN STD_LOGIC ;
 			clock0	: IN STD_LOGIC ;
-			address_a	: IN STD_LOGIC_VECTOR (6 DOWNTO 0);
+			address_a	: IN STD_LOGIC_VECTOR (9 DOWNTO 0);
 			q_a	: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 			data_a	: IN STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
@@ -97,13 +97,13 @@ BEGIN
 		intended_device_family => "Cyclone II",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 128,
+		numwords_a => 1024,
 		operation_mode => "SINGLE_PORT",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "UNREGISTERED",
 		power_up_uninitialized => "FALSE",
 		ram_block_type => "M4K",
-		widthad_a => 7,
+		widthad_a => 10,
 		width_a => 32,
 		width_byteena_a => 1
 	)
@@ -143,7 +143,7 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 -- Retrieval info: PRIVATE: MIFfilename STRING "INSTRUCTION_CACHE.mif"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "128"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "1024"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "2"
 -- Retrieval info: PRIVATE: READ_DURING_WRITE_MODE_PORT_A NUMERIC "3"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -153,7 +153,7 @@ END SYN;
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 -- Retrieval info: PRIVATE: UseDQRAM NUMERIC "1"
 -- Retrieval info: PRIVATE: WRCONTROL_ACLR_A NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "7"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "10"
 -- Retrieval info: PRIVATE: WidthData NUMERIC "32"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "NORMAL"
@@ -162,22 +162,22 @@ END SYN;
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "128"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "1024"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "SINGLE_PORT"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
 -- Retrieval info: CONSTANT: POWER_UP_UNINITIALIZED STRING "FALSE"
 -- Retrieval info: CONSTANT: RAM_BLOCK_TYPE STRING "M4K"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "7"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "10"
 -- Retrieval info: CONSTANT: WIDTH_A NUMERIC "32"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 7 0 INPUT NODEFVAL address[6..0]
+-- Retrieval info: USED_PORT: address 0 0 10 0 INPUT NODEFVAL address[9..0]
 -- Retrieval info: USED_PORT: clken 0 0 0 0 INPUT VCC clken
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC clock
 -- Retrieval info: USED_PORT: data 0 0 32 0 INPUT NODEFVAL data[31..0]
 -- Retrieval info: USED_PORT: q 0 0 32 0 OUTPUT NODEFVAL q[31..0]
 -- Retrieval info: USED_PORT: wren 0 0 0 0 INPUT NODEFVAL wren
--- Retrieval info: CONNECT: @address_a 0 0 7 0 address 0 0 7 0
+-- Retrieval info: CONNECT: @address_a 0 0 10 0 address 0 0 10 0
 -- Retrieval info: CONNECT: q 0 0 32 0 @q_a 0 0 32 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: @clocken0 0 0 0 0 clken 0 0 0 0
