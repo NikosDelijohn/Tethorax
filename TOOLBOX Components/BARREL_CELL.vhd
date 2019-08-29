c109dd48ddc5c310a285621e93b3e7e9d042b674
Nick Deligiannis <nik.deligi@gmail.com>
2019-08-29 23:51:35 +0300

    :wrench: Building Block of the Barrel Shifter

--- TOOLBOX Components/BARREL_CELL.vhd
+++ TOOLBOX Components/BARREL_CELL.vhd
@@ -11,13 +11,13 @@ USE WORK.TOOLBOX.ALL;
 
 ENTITY BARREL_CELL IS
 				
-	PORT (
-			D0    : IN  STD_LOGIC;
-			D1    : IN  STD_LOGIC;
-			D2    : IN  STD_LOGIC;
-			SEL   : IN  STD_LOGIC_VECTOR(1 DOWNTO 0);
-			O     : OUT STD_LOGIC
-		 );
+	PORT(
+		D0    : IN  STD_LOGIC;
+		D1    : IN  STD_LOGIC;
+		D2    : IN  STD_LOGIC;
+		SEL   : IN  STD_LOGIC_VECTOR(1 DOWNTO 0);
+		O     : OUT STD_LOGIC
+	    );
 
 END BARREL_CELL;
 
@@ -29,22 +29,21 @@ ARCHITECTURE STRUCTURAL OF BARREL_CELL IS
 	BEGIN
 
 		MUX2X1_A : MUX2X1_BIT
-				   PORT    MAP ( 
-								 D0  => D0,
-								 D1  => D1,
-								 SEL => SEL(0),
-								 O   => BUF_A
-								);
+		        PORT    MAP( 
+					D0  => D0,
+					D1  => D1,
+					SEL => SEL(0),
+					O   => BUF_A
+			           );
 								
 		MUX2X1_B : MUX2X1_BIT
-				   PORT    MAP (
-								 D0  => BUF_A,
-								 D1  => D2,
-								 SEL => SEL(1),
-								 O   => BUF_B
-								);
+			PORT    MAP(
+			                D0  => BUF_A,
+					D1  => D2,
+					SEL => SEL(1),
+					O   => BUF_B
+				   );
 		
 		O <= BUF_B;
 		
 END STRUCTURAL;
-	
